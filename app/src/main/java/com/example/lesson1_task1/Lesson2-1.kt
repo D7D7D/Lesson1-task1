c87c34c28d39a1cb840e40698b622b306773041b
dm <mahnod@mail.ru>
2023-07-10 20:23:54 +0700

    KS-2-1 arithmetic mean

---
+++ app/src/main/java/com/example/lesson1_task1/Lesson2-1.kt
@@ -0,0 +1,12 @@
+package com.example.lesson1_task1
+
+fun main() {
+    val student1 = 3
+    val student2 = 4
+    val student3 = 3
+    val student4 = 5
+
+    val result = (student1+student2+student3+student4)/4.0
+
+    println(result)
+}
