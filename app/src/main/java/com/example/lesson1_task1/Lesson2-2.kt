3584981383b5a6e4b12460b3ab9000b2686cb473
dm <mahnod@mail.ru>
2023-07-10 20:40:24 +0700

    KS-2-1 arithmetic mean

---
+++ app/src/main/java/com/example/lesson1_task1/Lesson2-2.kt
@@ -0,0 +1,19 @@
+package com.example.lesson1_task1
+
+fun main() {
+    val quantityWorkers = 50
+    val mainSalary = 30000
+
+    val quantityIntern = 30
+    val secondSalary = 20000
+
+    val sumMain = quantityWorkers * mainSalary
+    val sumSecond = quantityIntern * secondSalary + sumMain
+
+    val sum = sumSecond / (quantityWorkers + quantityIntern)
+
+    println("Расходы на выплату зарплаты постоянных сотрудников: $sumMain")
+    println("Общие расходы по ЗП после прихода стажеров: $sumSecond")
+    println("Средняя ЗП одного сотрудника после устройства стажеров: $sum")
+
+}
