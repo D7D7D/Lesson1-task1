362ca100fddb1ef9ada63b7270d13aa0f30412d3
dm <mahnod@mail.ru>
2023-07-13 22:06:26 +0700

    KS-4-2 - farm

---
+++ app/src/main/java/com/example/lesson1_task1/L4-3.kt
@@ -0,0 +1,15 @@
+package com.example.lesson1_task1
+
+fun main() {
+    val weather = true
+    val tent = true
+    val space = 20
+    val summer = "Лето"
+
+    val weather1 = true
+    val tent1 = true
+    val space1 = 20
+    val summer1 = "Лето"
+
+    println(weather1 == weather && tent == tent1 && space1 >= space && summer1 == summer)
+}
