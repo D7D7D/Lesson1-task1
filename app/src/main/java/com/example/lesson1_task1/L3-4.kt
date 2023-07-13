a0dc19dc70776ed0874861f615bd555633ede741
dm <mahnod@mail.ru>
2023-07-13 11:48:24 +0700

    KS-3-3 - Calculater

---
+++ app/src/main/java/com/example/lesson1_task1/L3-4.kt
@@ -0,0 +1,11 @@
+package com.example.lesson1_task1
+
+fun main() {
+    val from = "D2"
+    val inn = "D3"
+    val num = 2
+
+    //решенеи
+    val resultString = "$from - $inn; $num"
+    println(resultString)
+}
