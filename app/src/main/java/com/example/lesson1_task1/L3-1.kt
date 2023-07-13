7074421a7a93a565b1770aff8249f16335d83953
dm <mahnod@mail.ru>
2023-07-13 11:17:17 +0700

    KS-3-1 - Greeting

---
+++ app/src/main/java/com/example/lesson1_task1/L3-1.kt
@@ -0,0 +1,13 @@
+package com.example.lesson1_task1
+
+fun main() {
+    val greeting = "Hello"
+    val name = "Volodiy"
+
+    //решение
+    var result = "$greeting $name, сейчас день"
+    println(result)
+    result = "$greeting $name, сейчас ночь"
+    println(result)
+
+}
